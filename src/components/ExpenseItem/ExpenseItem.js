50c7c9cde1bac4a16b7b10aae85d6771e21c16b4
Alif Razan Saputra <alifrazanit@gmail.com>
2023-04-28 08:26:28 +0700

    useState

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,15 +1,22 @@
 import { ExpenseDate } from '../ExpenseDate/ExpenseDate';
 import './ExpenseItem.css';
 import { Card }from '../Card/Card';
+import { useState } from 'react';
 
 export const ExpenseItem = props => {
+    const [title, setTile] = useState('');
+
+    const changeTitle = e => {
+        setTile('zzzz')
+    }
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={changeTitle}>Change</button>
         </Card>
     )
 }
